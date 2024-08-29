5a9fcd9d06c1686847a24b1423ec995bf003f75b
cessy29 <eprincessroani@gmail.com>
2024-08-29 21:03:54 +0800

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Initialize Typed.js
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: ['Graphic Design', 'Web Design', 'Web Development', 'Software Development'],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 1000,
+        startDelay: 500,
+        loop: true
+    };
+    new Typed('#animated-text', options);
+});
